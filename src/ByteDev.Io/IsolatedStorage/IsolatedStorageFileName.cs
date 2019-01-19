02085a3646943dffd057b9921b4fd0890b7a0a45
Paul Stanley <paulstanley@protonmail.com>
2019-01-19 17:22:40 +0000

    Added simple IsolatedStorage functionality

---
+++ src/ByteDev.Io/IsolatedStorage/IsolatedStorageFileName.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ByteDev.Io.IsolatedStorage
+{
+    public class IsolatedStorageFileName
+    {
+        public IsolatedStorageFileName(string appName, Version version) : this(appName, version, string.Empty)
+        {
+        }
+
+        public IsolatedStorageFileName(string appName, Version version, string fileType)
+        {
+            AppName = appName;
+            Version = version;
+            FileType = fileType;
+        }
+
+        public Version Version { get; }
+
+        public string FileType { get; }
+
+        public string AppName { get; }
+
+        public string Name => $"{AppName}_{Version.Major}.{Version.Minor}{GetExtension(FileType)}";
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        private static string GetExtension(string fileType)
+        {
+            if (string.IsNullOrEmpty(fileType))
+                return string.Empty;
+
+            if (fileType.Substring(0, 1) != ".")
+            {
+                fileType = "." + fileType;
+            }
+            return fileType;
+        }
+    }
+}
---
+++ src/ByteDev.Io/IsolatedStorage/IsolatedStorageIo.cs
@@ -0,0 +1,68 @@
+﻿using System.IO;
+using System.IO.IsolatedStorage;
+using System.Xml;
+
+namespace ByteDev.Io.IsolatedStorage
+{
+    public class IsolatedStorageIo
+    {
+        public bool Exists(IsolatedStorageFileName fileName)
+        {
+            using (var file = GetStorageFile())
+            {
+                return file.FileExists(fileName.Name);
+            }
+        }
+
+        public void Delete(IsolatedStorageFileName fileName)
+        {
+            using (var file = GetStorageFile())
+            {
+                file.DeleteFile(fileName.Name);
+            }
+        }
+
+        public void Write(IsolatedStorageFileName fileName, XmlDocument xmlDocument)
+        {
+            Write(fileName, xmlDocument.OuterXml);
+        }
+
+        public void Write(IsolatedStorageFileName fileName, string content)
+        {
+            IsolatedStorageFile file = GetStorageFile();
+            
+            using (var stream = new IsolatedStorageFileStream(fileName.Name, FileMode.Create, file))
+            {
+                using (var writer = new StreamWriter(stream))
+                {
+                    writer.Write(content);
+                }
+            }
+        }
+
+        public XmlDocument ReadAsXml(IsolatedStorageFileName fileName)
+        {
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(Read(fileName));
+            return xmlDoc;
+        }
+
+        public string Read(IsolatedStorageFileName fileName)
+        {
+            IsolatedStorageFile file = GetStorageFile();
+
+            using (var stream = new IsolatedStorageFileStream(fileName.Name, FileMode.Open, file))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        private static IsolatedStorageFile GetStorageFile()
+        {
+            return IsolatedStorageFile.GetUserStoreForApplication();
+        }
+    }
+}
